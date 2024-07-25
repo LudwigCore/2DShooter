93e3371207734772443dc8261add91a099e11a95
ludwig meiton <moltenmagnesiumcore@gmail.com>
2024-07-25 19:03:08 +0200

    Entity Values

---
+++ Assets/EntityHolder/EntityVeriables.cs
@@ -0,0 +1,31 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+public class EntityVariables : MonoBehaviour
+{
+    // Health Stats
+    public int Health;
+    public int MaxHealth;
+    public string Resistance;
+    public string Weakness;
+    // Armor Stats
+    public int Armor;
+    public int MaxArmor;
+    public string ArmorResistance;
+    public string ArmorWeakness;
+    // Defense Stats
+    public float Defense;
+    public string DefenseResistance;
+    public string DefenseWeakness;
+    // Damage Stats
+    public float Damage;
+    public float ArmorPenetration;
+    public string DamageType;
+    // Movement Stats
+    public float MovementSpeed;
+    public float MaxSpeed;
+    // Weapon Stats
+    public string Weapon;
+    public int Ammo;
+    public int MaxAmmo;
+    public string AmmoType;
+}
