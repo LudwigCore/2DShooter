d968765b0e4deb5710ae9aebaa036075513c958c
ludwig meiton <moltenmagnesiumcore@gmail.com>
2024-07-26 11:07:54 +0200

    Never mind Cube Grid it is

---
+++ Assets/Scripts/DungeonHolder/DungeonGenerator.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+public class DungeonGenerator : MonoBehaviour
+{
+    public float RoomSize;
+    public int GridSize;
+    public Vector2[,] GridVector;
+    public GameObject TestObject;
+    void Start()
+    {
+        ClearDungeon();
+        GridGenerator();
+    }
+    void ClearDungeon()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("DungeonPart");
+        foreach (var item in obj)
+        {
+            Destroy(item);
+        }
+    }
+    void GridGenerator()
+    {
+        GridVector = new Vector2[GridSize, GridSize];
+        for (int x = 0; x < GridSize; x++)
+        {
+            for (int y = 0; y < GridSize; y++)
+            {
+
+            }
+        }
+    }
+    void dungeonGenerator()
+    {
+
+    }
+}
